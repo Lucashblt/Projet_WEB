faf2b7844861efea0f99704151b87bcd9a38228f
Lucashblt <121292238+Lucashblt@users.noreply.github.com>
2023-10-14 11:16:29 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -47,21 +47,29 @@ if($_POST) {
     <script> 
 
     function CheckLoginForm(){
-			var password = document.getElementById("password").value;
-			var confirm_password = document.getElementById("confirm_password").value;
-
-			if(password.length < 6){
-				alert("Les noms mots de passe de moins de 4 lettres ne sont pas autorisés!")
-				return false;
-			}
-			else if(password == confirm_password){
-				alert("Le mot de passe doit etre le même")
-				return false;
-			}
-			else {
-				return true;
-			}
+		var password = document.getElementById("password").value;
+		var confirm_password = document.getElementById("confirm_password").value;
+		var age = document.getElementById("date_naissance").value;
+		var today = new Date();
+		var email = document.getElementById("email").value;
+		var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+	
+		if(password.length < 6){
+			alert("Les noms mots de passe de moins de 4 lettres ne sont pas autorisés!")
+			return false;
+		}else if(password !== confirm_password){
+			alert("Le mot de passe doit etre le même")
+			return false;
+		}else if (age < 16) {
+			alert("Vous devez avoir au moins 16 ans pour vous inscrire.");
+			return false;
+		}else if (!email.match(emailPattern)) {
+			alert("Veuillez entrer une adresse email valide.");
+			return false;
+		}else{
+			return true;
 		}
+	}
     </script>
 </body>
 </html>
