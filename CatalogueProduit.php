1414828c5cad41bb19c022be092bbe765fa1cee5
Lucas <121292238+Lucashblt@users.noreply.github.com>
2023-10-30 17:09:41 +0100

    Add files via upload

--- CatalogueProduit.php
+++ CatalogueProduit.php
@@ -1,6 +1,8 @@
 <?php
     //Initialise la constante ROOT et $SQLconn pour la BDD
     include("./initialize.php");
+
+    include("./affichageproduit.php")
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./styles/CatalogueProduit.css">
+    <link rel="stylesheet" type="text/css" href="./styles/affichageproduit.css">
     <title>Catalogue Produit</title>
 </head>
 <body>
@@ -17,7 +20,7 @@
     <div class="filtres">
         <button class="filter-button">Filtrer</button>
         <div class="filter-options">
-            <form action="boutique.php" method="post">
+            <form action="CatalogueProduit.php" method="post">
                 <div class="filter-group">
                     <label for="categorie">Catégorie</label>
                     <select name="categorie" id="categorie">
@@ -55,42 +58,14 @@
     </div>
     <div class="products">
         <a href="produits.php" >
-            <div class="card">
-                <div class="poster"><img src="https://img.freepik.com/free-photo/men-s-apparel-hoodie-rear-view_53876-97228.jpg?w=360&t=st=1697035828~exp=1697036428~hmac=83f3871a04b294558c4708ee01250a26909b335f768df5b39d68850672781629" alt="Location Unknown"></div>
-                <div class="details">
-                    <h1>Dream And Reality</h1>
-                    <div class="tags">
-                        <span class="tag">White</span>
-                        <span class="tag">Black</span>
-                    </div>
-                    <h2>50€</h2>
-                </div>
-            </div>
+            <?php
+                echo createProductCard("Dream And Reality", "./img/imgBDD/produit1.jpg", ["White", "Black"], "50€");
+            ?>
         </a>
-        <div class="card">
-            <div class="poster"><img src="https://images.unsplash.com/photo-1560547126-ccd9d56db8af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80" alt="Location Unknown"></div>
-            <div class="details">
-                <h1>#Wolf</h1>
-                <div class="tags">
-                    <span class="tag">Blue</span>
-                    <span class="tag">White</span>
-                    <span class="tag">Green</span>
-                </div>
-                <h2>60€</h2>
-            </div>
-        </div>
-        <div class="card">
-            <div class="poster"><img src="https://img.freepik.com/free-photo/close-up-portrait-man-shirt-mockup_23-2149260967.jpg?w=360&t=st=1697036188~exp=1697036788~hmac=527d0d3eb9219978178f8820f6da2c869d4d54ad52ccc9496d05b6ca545744b5" alt="Location Unknown"></div>
-            <div class="details">
-                <h1>Sweatshirt en lain</h1>
-                <div class="tags">
-                    <span class="tag yellow">Grey</span>
-                    <span class="tag">Red</span>
-                    <span class="tag blue">Black</span>
-                </div>
-                <h2>70€</h2>
-            </div>
-        </div>
+        <?php
+            echo createProductCard("#Wolf", "https://images.unsplash.com/photo-1560547126-ccd9d56db8af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80", ["Blue", "White", "Green"], "60€");
+            echo createProductCard("Sweatshirt en lain", "https://img.freepik.com/free-photo/close-up-portrait-man-shirt-mockup_23-2149260967.jpg?w=360&t=st=1697036188~exp=1697036788~hmac=527d0d3eb9219978178f8820f6da2c869d4d54ad52ccc9496d05b6ca545744b5", ["Grey", "Red", "Black"], "70€");
+        ?>
     </div>
 
     <div class="number">
--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php
-//Initialise la constante ROOT et $SQLconn pour la BDD
-include("./initialize.php"); 
-
+    //Initialise la constante ROOT et $SQLconn pour la BDD
+    include("./initialize.php");
+    include("./affichageproduit.php") 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,13 +9,14 @@ include("./initialize.php");
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./styles/home.css">
+        <link rel="stylesheet" href="./styles/affichageproduit.css">
         <title>Accueil</title>
     </head>
     <body>
     <?php
         include('navbar.php');
     ?>
-        <div class="acceuil"> 
+        <div class="acceuil">
             <h1> Bienvenue sur Woolify 
                 <?php 
                     $queryPseudo = "SELECT pseudo FROM utilisateur WHERE email = '" . $SQLconn->loginStatus->userEmail . "'";
@@ -31,44 +32,11 @@ include("./initialize.php");
             <button  onclick="window.location.href ='boutique.php'"><span> J'en profite </span></button>
         </div>
         <div class="products">
-            <!-- 3 random products -->
-            <a href="produits.php" >
-                <div class="card">
-                    <div class="poster"><img src="https://img.freepik.com/free-photo/men-s-apparel-hoodie-rear-view_53876-97228.jpg?w=360&t=st=1697035828~exp=1697036428~hmac=83f3871a04b294558c4708ee01250a26909b335f768df5b39d68850672781629" alt="Location Unknown"></div>
-                    <div class="details">
-                        <h1>Dream And Reality</h1>
-                        <div class="tags">
-                            <span class="tag">White</span>
-                            <span class="tag">Black</span>
-                        </div>
-                        <h2>50€</h2>
-                    </div>
-                </div>
-            </a>
-            <div class="card">
-                <div class="poster"><img src="https://images.unsplash.com/photo-1560547126-ccd9d56db8af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80" alt="Location Unknown"></div>
-                <div class="details">
-                    <h1>#Wolf</h1>
-                    <div class="tags">
-                        <span class="tag">Blue</span>
-                        <span class="tag">White</span>
-                        <span class="tag">Green</span>
-                    </div>
-                    <h2>60€</h2>
-                </div>
-            </div>
-            <div class="card">
-                <div class="poster"><img src="https://img.freepik.com/free-photo/close-up-portrait-man-shirt-mockup_23-2149260967.jpg?w=360&t=st=1697036188~exp=1697036788~hmac=527d0d3eb9219978178f8820f6da2c869d4d54ad52ccc9496d05b6ca545744b5" alt="Location Unknown"></div>
-                <div class="details">
-                    <h1>Sweatshirt en lain</h1>
-                    <div class="tags">
-                        <span class="tag yellow">Grey</span>
-                        <span class="tag">Red</span>
-                        <span class="tag blue">Black</span>
-                    </div>
-                    <h2>70€</h2>
-                </div>
-            </div>
+            <?php
+                echo createProductCard("Selfie", "./img/imgBDD/vetement1.jpg", ["Blanc", "Noir"], "50€");
+                echo createProductCard("#Wolf", "./img/imgBDD/vetement2.jpg", ["Blue", "White", "Green"], "60€");
+                echo createProductCard("Pull The North Face", "./img/imgBDD/vetement3.png", ["Violet"], "70€");
+            ?>
         </div>
         <div class="Quality">
             <div class="column">
@@ -89,37 +57,11 @@ include("./initialize.php");
         </div>
 
         <div class="products">
-            <div class="card">
-                <div class="poster"><img src="https://images.unsplash.com/photo-1631167986287-1c3e6dd0597c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" alt="Location Unknown"></div>
-                <div class="details">
-                    <h1>Nike</h1>
-                    <div class="tags">
-                        <span class="tag">White</span>
-                        <span class="tag">Red</span>
-                    </div>
-                    <h2>100€</h2>
-                </div>
-            </div>
-            <div class="card">
-                <div class="poster"><img src="https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2012&q=80" alt="Location Unknown"></div>
-                <div class="details">
-                    <h1>Nike x Carhartt</h1>
-                    <div class="tags">
-                        <span class="tag">Brown</span>
-                    </div>
-                    <h2>200€</h2>
-                </div>
-            </div>
-            <div class="card">
-                <div class="poster"><img src="https://images.unsplash.com/photo-1595909236612-9fd30b476365?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" alt="Location Unknown"></div>
-                <div class="details">
-                    <h1>Nike Jordan</h1>
-                    <div class="tags">
-                        <span class="tag">Yellow and black</span>
-                    </div>
-                    <h2>120€</h2>
-                </div>
-            </div>
+            <?php
+                echo createProductCard("Nike x Carhart", "./img/imgBDD/chaussure1.jpg", ["Marron"], "150€");
+                echo createProductCard("Nike Air", "./img/imgBDD/chaussure2.jpg", ["Blanc", "Jaune"], "100€");
+                echo createProductCard("Timberland", "./img/imgBDD/chaussure3.jpg", ["Marron"], "180€")
+            ?>
         </div>
 
         <div class="Avis">
@@ -127,8 +69,8 @@ include("./initialize.php");
                 <div class="avis-header">
                     <span class="user">Utilisateur 1</span>
                     <span class="rating">
-                        <span class="stars">★★★★☆☆</span>
-                        <span class="note">4.0</span>
+                        <span class="stars">★★★☆☆</span>
+                        <span class="note">3.0</span>
                     </span>
                 </div>
                 <p class="commentaire">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam sapien nec nisi vestibulum, etiam cursus nulla eu sapien volutpat. Nulla dapibus felis eu orci blandit.<br><i>Avis laissé pour le produit XYZ</i></p>
